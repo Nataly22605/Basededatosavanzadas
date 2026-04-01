d3bcaa8151dc3da835de5552aa54fc95c3cf2352
Neyra <neyranatalyflorespena@gmial.com>
2026-04-01 16:54:37 -0600

    Agregue Ejercicio de Store para proyecto

---
+++ 02-ProgramacionProcedimentalconSQL/04-Proyecto-SP-WindowsForms.sql
@@ -0,0 +1,105 @@
+USE Northwind;
+
+
+--Crear tabla para DetalleVentaType
+CREATE TYPE DetalleVentaType AS TABLE
+(
+    IdProducto INT,
+    Cantidad INT
+);
+GO
+
+--Crear procedimiento almacenado para registrar una venta(varios productos)
+CREATE PROCEDURE spu_Registrar_Venta_Multiple
+    @IdCliente NCHAR(5),
+    @Detalle DetalleVentaType READONLY
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    BEGIN TRY
+        BEGIN TRANSACTION;
+
+        -- Validacion de cliente  existente 
+        IF NOT EXISTS (SELECT 1 FROM Cliente WHERE IdCliente = @IdCliente)
+        BEGIN
+            RAISERROR('El cliente no existe.',16,1); --Mensaje de error personalizado
+            ROLLBACK; 
+            RETURN;
+        END
+
+        -- Validacion de productos  y stock suficiente  
+        IF EXISTS (
+            SELECT d.IdProducto
+            FROM @Detalle d
+            LEFT JOIN Producto p ON d.IdProducto = p.IdProducto
+            WHERE p.IdProducto IS NULL
+        )
+        BEGIN
+            RAISERROR('Uno o más productos no existen.',16,1);
+            ROLLBACK;
+            RETURN;
+        END
+
+        IF EXISTS (
+            SELECT 1
+            FROM @Detalle d
+            JOIN Producto p ON d.IdProducto = p.IdProducto 
+            WHERE p.Existencia < d.Cantidad
+        )
+        BEGIN
+            RAISERROR('Stock insuficiente en uno o más productos.',16,1);
+            ROLLBACK;
+            RETURN;
+        END
+
+        -- Insertar venta y obtener el IdVenta 
+        DECLARE @IdVenta INT;
+
+        INSERT INTO Venta (Fecha, IdCliente)
+        VALUES (GETDATE(), @IdCliente);
+
+        SET @IdVenta = SCOPE_IDENTITY();--obtener el ultimo id insertado en la tabla Venta
+
+        -- Insertar detalla,para varios productos 
+        INSERT INTO DetalleVenta (IdVenta, IdProducto, PrecioVenta, Cantidad)
+        SELECT 
+            @IdVenta,
+            p.IdProducto,
+            p.Precio,
+            d.Cantidad
+        FROM @Detalle d
+        JOIN Producto p ON d.IdProducto = p.IdProducto;
+
+        -- Actualizar stock de productos
+        UPDATE p
+        SET p.Existencia = p.Existencia - d.Cantidad
+        FROM Producto p
+        JOIN @Detalle d ON p.IdProducto = d.IdProducto;
+
+        COMMIT;--La transacción se completo exitosamente
+
+        PRINT 'Venta registrada correctamente';
+    END TRY
+    BEGIN CATCH
+        IF @@TRANCOUNT > 0 ROLLBACK;--Revertir transaccion si hay error 
+
+        DECLARE @Error NVARCHAR(4000) = ERROR_MESSAGE();
+        RAISERROR(@Error,16,1);
+    END CATCH
+END;
+GO
+
+------------Pueba de Procedimiento almacenado------------
+
+DECLARE @Detalle DetalleVentaType;
+                            --IdVenta,IdProducto
+INSERT INTO @Detalle VALUES (1,2);
+INSERT INTO @Detalle VALUES (2,3);
+INSERT INTO @Detalle VALUES (3,1);
+
+
+EXEC spu_Registrar_Venta_Multiple
+    @IdCliente = 'BOTTM',
+    @Detalle = @Detalle;
+-----------------------------------------------------------
